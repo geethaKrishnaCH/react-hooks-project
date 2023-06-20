912dc07a5971497cc076bca1b2c13c1bb937015b
Geetha <chgeethakrishna@gmail.com>
2023-06-20 11:30:53 +0530

    form validation draft

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,33 +3,35 @@ import Card from "../UI/Card/Card";
 import styles from "./Login.module.css";
 import Button from "../UI/Button/Button";
 function Login({ onLogin }) {
+  const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   const [user, setUser] = useState({ email: "", password: "" });
   const [formIsValid, setFormIsValid] = useState(false);
-  const [emailIsValid, setEmailIsValid] = useState(false);
-  const [passwordIsValid, setPasswordIsValid] = useState(false);
-
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [passwordIsValid, setPasswordIsValid] = useState();
 
   function handleEmailChange(event) {
     setUser({ ...user, email: event.target.value });
     setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
+      event.target.value.match(mailformat) && user.password.trim().length > 6
     );
   }
 
   function handlePasswordChange(event) {
     setUser({ ...user, password: event.target.value });
     setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+      event.target.value.trim().length > 6 && user.email.match(mailformat)
     );
   }
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(user.email.match(mailformat));
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    setPasswordIsValid(user.password.trim().length > 6);
   };
+
+  console.log(emailIsValid === false, passwordIsValid);
 
   return (
     <Card cssClasses={styles["form-container"]}>
@@ -38,15 +40,19 @@ function Login({ onLogin }) {
         <input
           type="text"
           name="email"
+          className={emailIsValid === true ? "" : styles.invalid}
           onChange={handleEmailChange}
           value={user.email}
+          onBlur={validateEmailHandler}
         />
         <label htmlFor="password">Password</label>
         <input
           type="password"
           name="password"
+          className={passwordIsValid === true ? "" : styles.invalid}
           onChange={handlePasswordChange}
           value={user.password}
+          onBlur={validatePasswordHandler}
         />
         <Button onClick={onLogin} disabled={!formIsValid}>
           Login
